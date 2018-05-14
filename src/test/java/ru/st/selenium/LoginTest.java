17699a5a9519cb3544bada2da5dcd896febb00c3
Anton <feodan@yandex.ru>
2018-05-14 15:17:02 +0300

    init commit

--- src/test/java/ru/st/selenium/LoginTest.java
+++ src/test/java/ru/st/selenium/LoginTest.java
@@ -1,32 +1,82 @@
 package ru.st.selenium;
 
-import org.testng.annotations.*;
-import static org.junit.Assert.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.annotations.Test;
+import ru.st.selenium.pages.TestBase;
 
-import ru.st.selenium.model.User;
+public class LoginTest extends TestBase {
 
-public class LoginTest extends ru.st.selenium.pages.TestBase {
+//  @BeforeMethod
+//  public void mayBeLogout() {
+//    if (app.getUserHelper().isLoggedIn()) {
+//      app.getUserHelper().logout();
+//    }
+//  }
+//
+//  @Test
+//  public void testLoginOK() throws Exception {
+//    User user = new User().setLogin("admin").setPassword("admin");
+//    app.getUserHelper().loginAs(user);
+//    assertTrue(app.getUserHelper().isLoggedInAs(user));
+//  }
+//
+//  @Test
+//  public void testLoginFailed() throws Exception {
+//    User user = new User().setLogin("admin").setPassword("wrong");
+//    app.getUserHelper().loginAs(user);
+//    // assertFalse(app.getUserHelper().isLoggedIn());
+//    assertTrue(app.getUserHelper().isNotLoggedIn());
+//  }
 
-  @BeforeMethod
-  public void mayBeLogout() {
-    if (app.getUserHelper().isLoggedIn()) {
-      app.getUserHelper().logout();
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Test
+    public void testUntitled() throws InterruptedException {
+        driver.get(baseUrl + "/");
+        driver.findElement(By.linkText("войдите в личный кабинет")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.name("login")).clear();
+        driver.findElement(By.name("login")).sendKeys("feodan@yandex.ru");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("co454540");
+        driver.findElement(By.name("auth_submit")).click();
     }
-  }
-  
-  @Test
-  public void testLoginOK() throws Exception {
-    User user = new User().setLogin("admin").setPassword("admin");
-    app.getUserHelper().loginAs(user);
-    assertTrue(app.getUserHelper().isLoggedInAs(user));
-  }
 
-  @Test
-  public void testLoginFailed() throws Exception {
-    User user = new User().setLogin("admin").setPassword("wrong");
-    app.getUserHelper().loginAs(user);
-    // assertFalse(app.getUserHelper().isLoggedIn());
-    assertTrue(app.getUserHelper().isNotLoggedIn());
-  }
 
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
--- src/test/java/ru/st/selenium/pages/TestBase.java
+++ src/test/java/ru/st/selenium/pages/TestBase.java
@@ -1,69 +1,57 @@
 package ru.st.selenium.pages;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.ScreenshotException;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
-
-import ru.st.selenium.applogic.ApplicationManager;
-import ru.st.selenium.util.PropertyLoader;
 import ru.st.selenium.util.Browser;
+import ru.st.selenium.util.PropertyLoader;
 import ru.st.selenium.webdriver.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /*
  * Base class for all the test classes
- * 
+ *
  * @author Sebastiano Armeli-Battana
  */
 
 public class TestBase {
-	private static final String SCREENSHOT_FOLDER = "target/screenshots/";
-	private static final String SCREENSHOT_FORMAT = ".png";
+    private static final String SCREENSHOT_FOLDER = "target/screenshots/";
+    private static final String SCREENSHOT_FORMAT = ".png";
 
-	protected WebDriver driver;
+    protected WebDriver driver;
 
-	protected String gridHubUrl;
+    protected String gridHubUrl;
 
-	protected String baseUrl;
+    protected String baseUrl;
 
-	protected Browser browser;
-	
-	protected ApplicationManager app;
+    protected Browser browser;
 
-	@BeforeClass
-	public void init() {
-		baseUrl = PropertyLoader.loadProperty("site.url");
-		gridHubUrl = PropertyLoader.loadProperty("grid2.hub");
+//    protected ApplicationManager app;
 
-		browser = new Browser();
-		browser.setName(PropertyLoader.loadProperty("browser.name"));
-		browser.setVersion(PropertyLoader.loadProperty("browser.version"));
-		browser.setPlatform(PropertyLoader.loadProperty("browser.platform"));
+    @BeforeClass
+    public void init() {
+        baseUrl = PropertyLoader.loadProperty("site.url");
+        gridHubUrl = PropertyLoader.loadProperty("grid2.hub");
 
-		String username = PropertyLoader.loadProperty("user.username");
-		String password = PropertyLoader.loadProperty("user.password");
-		
-		driver = WebDriverFactory.getInstance(gridHubUrl, browser, username,
-				password);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	}
+        browser = new Browser();
+        browser.setName(PropertyLoader.loadProperty("browser.name"));
+        browser.setVersion(PropertyLoader.loadProperty("browser.version"));
+        browser.setPlatform(PropertyLoader.loadProperty("browser.platform"));
 
-	@AfterSuite(alwaysRun = true)
-	public void tearDown() {
-		if (driver != null) {
-			driver.quit();
-		}
-	}
+        String username = PropertyLoader.loadProperty("user.username");
+        String password = PropertyLoader.loadProperty("user.password");
+
+        driver = WebDriverFactory.getInstance(gridHubUrl, browser, username, password);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
 //	@AfterMethod
 //	public void setScreenshot(ITestResult result) {
